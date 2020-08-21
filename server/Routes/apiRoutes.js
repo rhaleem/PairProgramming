b9bd2e3e20445f6b3525b7b63b63ab2b9276bf6e
yashnagpaul <65575697+yashnagpaul@users.noreply.github.com>
2020-08-21 12:29:40 -0700

    made changes to apiRoutes and index.js in SERVER

--- server/Routes/apiRoutes.js
+++ server/Routes/apiRoutes.js
@@ -6,10 +6,28 @@ const fs = require("fs");
 const signedUpListFile = path.join(__dirname, "../data/signedUpList.json");
 const signedUpData = fs.readFileSync(signedUpListFile);
 
-router.post("/", addNewUser);
+router.post("/users", () => addNewUser);
+router.get("/users", () => console.log("get/users is working"));
 
 function addNewUser(req, res) {
   res.json({ status: "signed up" });
+
+  const usersArray = listUsers();
+
+  let newUser = {
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  };
+
+  usersArray.push(newUser);
+  fs.writeFileSync(signedUpListFile, JSON.stringify(usersArray));
+  return usersArray;
+}
+
+function listUsers() {
+  console.log("working");
+  return JSON.parse(signedUpData);
 }
 
 module.exports = router;
--- server/index.js
+++ server/index.js
@@ -3,8 +3,9 @@ const index = express();
 const apiRoutes = require("./Routes/apiRoutes.js");
 const cors = require("cors");
 
+index.get("/", (req, res) => res.send("<h1>Welcome to this API</h1>"));
 index.use(cors());
 index.use(express.json());
-index.use("/", apiRoutes);
+index.use("/users", apiRoutes);
 
 index.listen(5000, console.log("server running at http://localhost:5000"));
